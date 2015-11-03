fe0c9b3d17d1cc19d8043ae141f3a414d096fff7
Joydeep Roy Chowdhury <joydeeprc@gmail.com>
2015-11-03 08:31:12 +0530

    role popup, more data

--- ui/mockups/js/SalesCommission.js
+++ ui/mockups/js/SalesCommission.js
@@ -4,8 +4,7 @@ function showMenu() {
 	menuTxt += '<li class="menu"><a href="#">Master Data</a>';
 	menuTxt += '<ul class="menu"> ';
 	menuTxt += '<li class="menu"><a href="../employee/EmployeeList.html">Employee</a></li>';
-	menuTxt += '<li class="menu"><a href="#">Role</a></li>';
-	menuTxt += '<li class="menu"><a href="../orgStructure/OrgStructure.html">Org Structure</a></li>';
+	menuTxt += '<li class="menu"><a href="../orgStructure/OrgStructure.html">Employee Roles</a></li>';
 	menuTxt += '<li class="menu"><a href="../compRule/CompRule.html">Comp Rule</a></li>';
 	menuTxt += '<li class="menu"><a href="../rollup/RollupRule.html">Credit Rollup Rule</a></li>';
 	menuTxt += '</ul> ';
@@ -47,4 +46,11 @@ function printChooseCal() {
 	var empTxt = '';
 	empTxt += '<img onclick="window.open(\'../popups/calendar.html\', \'cal\', \'toolbar=no, location=no, status=no, menubar=no, scrollbars=no, resizable=no, height=200, width=200\')" src="../../image/calendar.png" style="width:30px;height:20px;"/>';
 	document.write(empTxt);
+}
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+						vars[key] = value;
+				});
+	return vars;
 }
