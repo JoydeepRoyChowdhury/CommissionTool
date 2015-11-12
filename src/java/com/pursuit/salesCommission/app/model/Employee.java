1cafe4452b3f534a34cf4fb3f72edc09fe100d21
Pramita <pramita.mail@gmail.com>
2015-11-12 11:47:07 +0530

    formatted

--- src/java/com/pursuit/salesCommission/app/model/Employee.java
+++ src/java/com/pursuit/salesCommission/app/model/Employee.java
@@ -1,28 +1,32 @@
 package com.pursuit.salesCommission.app.model;
 
 public class Employee {
-private int id;
-private String name;
-private float salary;
+	private int id;
+	private String name;
+	private float salary;
 
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public float getSalary() {
-	return salary;
-}
-public void setSalary(float salary) {
-	this.salary = salary;
-}
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
 
 }
