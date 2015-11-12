c205fe859cff34ca57c9a9e77b82bbb20ee30f86
Pramita <pramita.mail@gmail.com>
2015-11-12 11:48:28 +0530

    formatted

--- src/java/com/pursuit/salesCommission/app/api/dao/EmployeeDao.java
+++ src/java/com/pursuit/salesCommission/app/api/dao/EmployeeDao.java
@@ -1,4 +1,5 @@
 package com.pursuit.salesCommission.app.api.dao;
+
 import java.util.Collection;
 
 import org.springframework.orm.hibernate3.HibernateTemplate;
@@ -6,23 +7,25 @@ import org.springframework.orm.hibernate3.HibernateTemplate;
 import com.pursuit.salesCommission.app.model.Employee;
 
 public class EmployeeDao {
-HibernateTemplate template;
-public void setTemplate(HibernateTemplate template) {
-	this.template = template;
-}
+	HibernateTemplate template;
 
-public void saveEmployee(Employee e){
-	template.save(e);
-}
+	public void setTemplate(HibernateTemplate template) {
+		this.template = template;
+	}
 
-public void updateEmployee(Employee e){
-	template.update(e);
-}
+	public void saveEmployee(Employee e) {
+		template.save(e);
+	}
 
-public void deleteEmployee(Employee e){
-	template.delete(e);
-}
-public Collection<Employee> searchEmployee(Employee e){
-	return (Collection<Employee>) template.find("from Employee e");
-}
+	public void updateEmployee(Employee e) {
+		template.update(e);
+	}
+
+	public void deleteEmployee(Employee e) {
+		template.delete(e);
+	}
+
+	public Collection<Employee> searchEmployee(Employee e) {
+		return (Collection<Employee>) template.find("from Employee e");
+	}
 }
