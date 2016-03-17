eadaed7fff642d7a2acb3e8bccd640f040dd405f
SwarupKhan <swarup119khan@gmail.com>
2016-03-17 10:31:26 +0530

    parameterUI

---
+++ src/java/com/pursuit/salesCommission/app/model/ParameterUI.java
@@ -0,0 +1,29 @@
+package com.pursuit.salesCommission.app.model;
+
+import java.util.List;
+
+public class ParameterUI {
+	
+	    private List<ParameterUI> parameterName;
+	    
+	    private List<ParameterUI> parameterValue;
+    
+		public List<ParameterUI> getParameterValue() {
+			return parameterValue;
+		}
+
+		public void setParameterValue(List<ParameterUI> parameterValue) {
+			this.parameterValue = parameterValue;
+		}
+
+		public List<ParameterUI> getParameterName() {
+			return parameterName;
+		}
+
+		public void setParameterName(List<ParameterUI> parameterName) {
+			this.parameterName = parameterName;
+		}
+	
+
+
+}
