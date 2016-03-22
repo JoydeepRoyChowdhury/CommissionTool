178a69fcfacd6e19a9c746f12638be862b8a2140
rashid.reza_sit@yahoo.com <rashid.reza_sit@yahoo.com>
2016-03-22 16:58:41 +0530

    added js and css

---
+++ src/java/com/pursuit/salesCommission/app/model/SearchEmployee.java
@@ -0,0 +1,13 @@
+package com.pursuit.salesCommission.app.model;
+
+public class SearchEmployee {
+private String Employee;
+
+public String getEmployee() {
+	return Employee;
+}
+
+public void setEmployee(String employee) {
+	Employee = employee;
+}
+}
