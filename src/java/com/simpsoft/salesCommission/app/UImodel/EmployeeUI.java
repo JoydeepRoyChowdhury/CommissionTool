fcd6672d1155dc66d340e2ddb2ab9eb9fbfc170b
rashid.reza_sit@yahoo.com <rashid.reza_sit@yahoo.com>
2016-04-15 18:51:17 +0530

    Update employeeui roleui

---
+++ src/java/com/simpsoft/salesCommission/app/UImodel/EmployeeUI.java
@@ -0,0 +1,24 @@
+package com.simpsoft.salesCommission.app.UImodel;
+
+public class EmployeeUI {
+	
+	private String employeeName;
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	
+	public EmployeeUI(){		
+	}
+	
+	public EmployeeUI(String employeeName){
+		this.employeeName=employeeName;
+		
+		
+	}
+
+}
---
+++ src/java/com/simpsoft/salesCommission/app/UImodel/RoleUI.java
@@ -0,0 +1,23 @@
+package com.simpsoft.salesCommission.app.UImodel;
+
+public class RoleUI {
+	
+	private String roleName;
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public RoleUI(){		
+	}
+	
+	public RoleUI(String roleName){
+		this.roleName=roleName;
+		
+		
+	}
+}
